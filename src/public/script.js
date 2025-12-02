76f0b3541dc22f1f228db20e0576f78999fe857f
host2 <host2@domido.org>
2025-12-02 22:43:32 +0800

    debuging Private/workspace style

--- src/public/script.js
+++ src/public/script.js
@@ -3691,11 +3691,11 @@
 													<button class='edit icon-btn' title='Edit'>
 															<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'><path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'></path></svg>
 													</button>
-													<div class="visibility-selector">
-														<button type="button" class="visibility-selector-btn icon-btn" title="Change visibility">
+													<div class="visibility-selector" data-visibility="${note.visibility || 'private'}">
+														<button type="button" class="visibility-selector-btn text-btn" title="Change visibility">
 															<!-- Icon will be set by JS -->
 														</button>
-														<div class="popover-menu visibility-popover">
+														<div class="popover-menu visibility-popover" style="display: none;">
 															<div class="visibility-option" data-visibility="private">
 																<svg height="20px" viewBox="0 0 24 24" width="20px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM12 17c-.55 0-1-.45-1-1v-4c0-.55.45-1 1-1s1 .45 1 1v4c0 .55-.45 1-1 1zm1-9h-2V7h2v1z"/></svg>
 																<span>Private</span></div>
